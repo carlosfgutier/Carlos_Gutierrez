9a35c48fe96842f74c5a0fbbd287aad6076a70e3
Carlos <carlosfgutier@gmail.com>
2018-04-04 10:19:43 -0500

    simplified parallax code

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -5,18 +5,25 @@ $(window).scroll(function(e) {
 function parallax() {
 
 	var scroll = $(window).scrollTop();
-	var screenHeight = $(window).height();
 
-	console.log(scroll +' ----- '+ screenHeight);
-
-	$('.parallax-bg').each(function() {
-		var offset = $(this).offset().top;
-		var dft = offset - scroll - screenHeight;
-
-		if (offset > screenHeight && offset) {
-			$(this).css('background-position', 'center ' + (( -dft ) *0.5) + 'px');
-		} else {
-			$(this).css('background-position', 'center ' + (( -scroll ) *0.5) + 'px');
-		}
-	});
+	$('.parallax-bg').css('background-position', 'center ' +(scroll*0.75)+ 'px');
+	
 };
+
+
+
+	
+	// var screenHeight = $(window).height();
+
+	// console.log(scroll +' ----- '+ screenHeight);
+
+	// $('.parallax-bg').each(function() {
+	// 	var offset = $(this).offset().top;
+	// 	var dft = offset - scroll - screenHeight;
+
+	// 	if (offset > screenHeight && offset) {
+	// 		$(this).css('background-position', 'center ' + (( -dft ) *0.5) + 'px');
+	// 	} else {
+	// 		$(this).css('background-position', 'center ' + (( -scroll ) *0.5) + 'px');
+	// 	}
+	// });
