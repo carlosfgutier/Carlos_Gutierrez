bff7c4ba5f8e7edd71885c0b70309829e5efe5cf
Carlos <carlosfgutier@gmail.com>
2018-04-13 15:23:56 -0500

    tested first parallax square

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -7,23 +7,7 @@ function parallax() {
 	var scroll = $(window).scrollTop();
 
 	$('.parallax-bg').css('background-position', 'center ' +(scroll*0.75)+ 'px');
+
+	$('#pinkSquare1').css('bottom', -100 -(scroll*0.75)+ 'px');
 	
 };
-
-
-
-	
-	// var screenHeight = $(window).height();
-
-	// console.log(scroll +' ----- '+ screenHeight);
-
-	// $('.parallax-bg').each(function() {
-	// 	var offset = $(this).offset().top;
-	// 	var dft = offset - scroll - screenHeight;
-
-	// 	if (offset > screenHeight && offset) {
-	// 		$(this).css('background-position', 'center ' + (( -dft ) *0.5) + 'px');
-	// 	} else {
-	// 		$(this).css('background-position', 'center ' + (( -scroll ) *0.5) + 'px');
-	// 	}
-	// });
