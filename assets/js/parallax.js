002327055a8036ab8ca74b3825376a35ec92f0bd
Carlos <carlosfgutier@gmail.com>
2018-04-13 18:05:59 -0500

    organized parallax code

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -6,8 +6,20 @@ function parallax() {
 
 	var scroll = $(window).scrollTop();
 
+	// mobile ft project
 	$('.parallax-bg').css('background-position', 'center ' +(scroll*0.75)+ 'px');
 
-	$('#pinkSquare1').css('bottom', -100 -(scroll*0.75)+ 'px');
+	// about me
+	$('#greenSquare1').css('bottom', -100 -(scroll*0.75)+ 'px');
+	$('#greenSquare1').css('right', (scroll*0.75)+ 'px');
+	$('#greenSquare1').css('transform', 'rotate(' + -2 *(scroll*0.105) + 'deg)');
+
+	// skills
+	$('#communication-bg').css('transform', 'rotate(' + 5+(scroll*0.025) + 'deg)');
+	$('#organization-bg').css('transform', 'rotate(' + -5+(scroll*0.025) + 'deg)');
+	$('#collaboration-bg').css('transform', 'rotate(' + 5+(scroll*0.025) + 'deg)');
+	$('#design-bg').css('transform', 'rotate(' + -5+(scroll*0.025) + 'deg)');
+	$('#time-bg').css('transform', 'rotate(' + 5+(scroll*0.025) + 'deg)');
+
 	
 };
